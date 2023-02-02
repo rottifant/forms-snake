0f7bd81fcf7e88ec5ba8eb5a1659aac72d93fb5b
Raphael Rott <raphael@rottbande.at>
2023-02-02 20:37:45 +0100

    init

---
+++ snake/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace snake
+{
+    partial class SnakeGame
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.newGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.score = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainTimer = new System.Windows.Forms.Timer(this.components);
+            this.board = new System.Windows.Forms.Panel();
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.mainLabel = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.board.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGame,
+            this.score});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(400, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // newGame
+            // 
+            this.newGame.Name = "newGame";
+            this.newGame.Size = new System.Drawing.Size(77, 20);
+            this.newGame.Text = "New Game";
+            this.newGame.Click += new System.EventHandler(this.NewGame);
+            // 
+            // score
+            // 
+            this.score.Enabled = false;
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(60, 20);
+            this.score.Text = "Score: 0";
+            // 
+            // mainTimer
+            // 
+            this.mainTimer.Interval = 200;
+            this.mainTimer.Tick += new System.EventHandler(this.MoveSnake);
+            // 
+            // board
+            // 
+            this.board.Controls.Add(this.instructionLabel);
+            this.board.Controls.Add(this.mainLabel);
+            this.board.Location = new System.Drawing.Point(0, 24);
+            this.board.Name = "board";
+            this.board.Size = new System.Drawing.Size(400, 400);
+            this.board.TabIndex = 1;
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.AutoSize = true;
+            this.instructionLabel.Location = new System.Drawing.Point(120, 215);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(160, 13);
+            this.instructionLabel.TabIndex = 1;
+            this.instructionLabel.Text = "Press Enter to start a new game!";
+            // 
+            // mainLabel
+            // 
+            this.mainLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mainLabel.Location = new System.Drawing.Point(0, 0);
+            this.mainLabel.Name = "mainLabel";
+            this.mainLabel.Size = new System.Drawing.Size(400, 400);
+            this.mainLabel.TabIndex = 0;
+            this.mainLabel.Text = "🐍 SNAKE 🐍";
+            this.mainLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SnakeGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 424);
+            this.Controls.Add(this.board);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "SnakeGame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Snake";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ChangeDirection);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.board.ResumeLayout(false);
+            this.board.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem newGame;
+        private System.Windows.Forms.Timer mainTimer;
+        private System.Windows.Forms.Panel board;
+        private System.Windows.Forms.ToolStripMenuItem score;
+        private System.Windows.Forms.Label mainLabel;
+        private System.Windows.Forms.Label instructionLabel;
+    }
+}
+
---
+++ snake/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace snake
+{
+    public partial class SnakeGame : Form
+    {
+
+        private enum SnakeDirection
+        {
+            Up,
+            Down,
+            Left,
+            Right,
+        }
+
+        SnakeDirection direction = SnakeDirection.Right;
+        List<PictureBox> snakePartsList = new List<PictureBox>();
+        PictureBox apple = new PictureBox();
+        Random random = new Random();
+        Image snakeHeadImage = Properties.Resources.SnakeHead;
+        bool gameRunning = false;
+
+        public SnakeGame()
+        {
+            InitializeComponent();
+        }
+
+        private void StartGame()
+        {
+            gameRunning = true;
+
+            foreach (PictureBox pictureBox in snakePartsList)
+            {
+                board.Controls.Remove(pictureBox);
+            }
+            snakePartsList.Clear();
+
+
+            direction = SnakeDirection.Right;
+
+            instructionLabel.Visible = false;
+            mainLabel.Visible = false;
+
+            // Initialize Apple
+            apple.Height = 18;
+            apple.Width = 18;
+            apple.BackColor = Color.IndianRed;
+            this.board.Controls.Add(apple);
+            RepositionApple();
+
+            // Draw first snake part
+            DrawNewSnakePart();
+
+            snakePartsList[0].BackgroundImage = snakeHeadImage;
+
+            // Update score and start game
+            UpdateScore();
+            mainTimer.Start();
+        }
+
+        private void MoveSnake(object sender, EventArgs e)
+        {
+            for (int i = snakePartsList.Count - 1; i > 0; i--)
+            {
+                snakePartsList[i].Left = snakePartsList[i - 1].Left;
+                snakePartsList[i].Top = snakePartsList[i - 1].Top;
+            }
+            if (direction == SnakeDirection.Up)
+                snakePartsList[0].Top -= 25;
+            else if (direction == SnakeDirection.Down)
+                snakePartsList[0].Top += 25;
+            else if (direction == SnakeDirection.Left)
+                snakePartsList[0].Left -= 25;
+            else
+                snakePartsList[0].Left += 25;
+
+            if (snakePartsList[0].Left > 400 || snakePartsList[0].Left < 0 || snakePartsList[0].Top > 400 || snakePartsList[0].Top < 0)
+            {
+                GameOver();
+                return;
+            }
+
+            if (apple.Bounds.IntersectsWith(snakePartsList[0].Bounds))
+            {
+                EatApple();
+                return;
+            }
+
+            for (int i = snakePartsList.Count - 1; i > 0; i--)
+            {
+                if (snakePartsList[i].Left == snakePartsList[0].Left && snakePartsList[i].Top == snakePartsList[0].Top)
+                    GameOver();
+            }
+        }
+
+        private void EatApple()
+        {
+            DrawNewSnakePart();
+            UpdateScore();
+            RepositionApple();
+        }
+
+        private void UpdateScore()
+        {
+            score.Text = "Score: " + (snakePartsList.Count - 1);
+        }
+
+        private void DrawNewSnakePart()
+        {
+            snakePartsList.Add(new PictureBox());
+            PictureBox snakePart = snakePartsList[snakePartsList.Count - 1];
+            snakePart.Height = 25;
+            snakePart.Width = 25;
+            if (snakePartsList.Count > 1)
+            {
+                snakePart.Left = snakePartsList[snakePartsList.Count - 2].Left;
+                snakePart.Top = snakePartsList[snakePartsList.Count - 2].Top;
+                snakePart.BackColor = Color.Green;
+            }
+            this.board.Controls.Add(snakePart);
+        }
+
+        private void RepositionApple()
+        {
+            apple.Left = random.Next(16) * 25 + 3;
+            apple.Top = random.Next(16) * 25 + 3;
+
+            foreach (PictureBox snakePart in snakePartsList)
+            {
+                if (apple.Bounds.IntersectsWith(snakePart.Bounds))
+                    RepositionApple();
+            }
+        }
+
+        private void GameOver()
+        {
+            gameRunning = false;
+            mainTimer.Stop();
+            mainLabel.Text = "Game over!";
+            mainLabel.Visible = true;
+            instructionLabel.Visible = true;
+        }
+
+        private void ChangeDirection(object sender, KeyEventArgs e)
+        {
+            snakeHeadImage = Properties.Resources.SnakeHead;
+
+
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    snakeHeadImage.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                    direction = SnakeDirection.Left;
+                    break;
+                case Keys.Right:
+                    snakeHeadImage.RotateFlip(RotateFlipType.RotateNoneFlipNone);
+                    direction = SnakeDirection.Right;
+                    break;
+                case Keys.Up:
+                    snakeHeadImage.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                    direction = SnakeDirection.Up;
+                    break;
+                case Keys.Down:
+                    snakeHeadImage.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                    direction = SnakeDirection.Down;
+                    break;
+                case Keys.Enter:
+                    if (!mainTimer.Enabled)
+                        StartGame();
+                    return;
+                case Keys.Escape:
+                    PauseGame();
+                    return;
+                default:
+                    return;
+            }
+
+            snakePartsList[0].BackgroundImage = snakeHeadImage;
+
+        }
+
+        private void PauseGame()
+        {
+            if (gameRunning && mainTimer.Enabled)
+            {
+                mainTimer.Stop();
+                mainLabel.Text = "Paused";
+                mainLabel.Visible = true;
+            }
+            else
+            {
+                mainTimer.Start();
+                mainLabel.Visible = false;
+            }
+        }
+
+        private void NewGame(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+    }
+}
